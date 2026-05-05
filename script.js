3c92244398605950a72d765724f55fd23cb47ed1
jacques <frichot.p@gmail.com>
2026-05-05 23:42:04 +1000

    change the way the book object is being pushed to the library array. Instead of pushing each property as a string, push the entire book object with its properties. This will make it easier to access the properties of each book later on.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
 
+let myLibrary = [];
+
 
 class Books {
-    constructor(name, title, author, pages, ids) {
+    constructor(name, title, author, pages) {
         this.name = name;
         this.title = title;
         this.author = author;
         this.pages = pages;
-        this.id = ids;
+       
     }
 }
 
-Books.prototype.bookLibrary = function() {
-    let bookEntry = [];
-    for(let id in bookEntry) {
-        bookEntry.push(this.id) = cripto.randomUUID;
-    }
-    
-    bookEntry.push(`name: ${this.name}`, `title: ${this.title}`, `author: ${this.author}`, `pages: ${this.pages}`, `id: ${this.id}`);
-    
-    console.log(bookEntry);
-    console.log(bookEntry);
+Books.prototype.addBookToLibrary = function() {
 
+        this.id = crypto.randomUUID();
+        myLibrary.push({name: this.name, title: this.title, author: this.author, pages: this.pages, id: this.id});
+        
+    console.log(myLibrary);  
 }
 
-const book1 = new Books("Harry Potter", "The prisoner of Azcabane", "JK Rowling", 200, this.id);
+const book1 = new Books("Harry Potter", "The prisoner of Azcabane", "JK Rowling", 200, "");
+const book2 = new Books("Lord of the Rings", "The Fellowship of the Ring", "J.R.R. Tolkein", 300, "");
 
 
-book1.bookLibrary();
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+
