bd7afe2117035e0cea2295d24bb61a61782eb41d
jacques <frichot.p@gmail.com>
2026-05-09 20:10:34 +1000

    completely refactor the javascript and implement the display and add new book

--- script.js
+++ script.js
@@ -1,72 +1,74 @@
+const display = document.getElementById("display-book");
+const form = document.getElementById("book-form");
+const openBtn = document.getElementById("openBtn");
+const dialog = document.getElementById("my-dialog");
+const bookNameInput = document.getElementById("book-name");
+const bookTitleInput = document.getElementById("book-title");
+const authorInput = document.getElementById("author");
+const bookPagesInput = document.getElementById("pages");
+const mySelection = document.getElementById("mySelection");
 
-const displayBooks = document.getElementById("displayBooks");
-const bookId = document.getElementById("bookId");
-
-
-
-
-let myLibrary = [
-    {name: "Harry Potter",   
-    title: "The prisoner of Azcabane",
-    author: "JK Rowling",
-    pages: 200},   
-
-    {name: "Lord of the Rings",
-    title: "The Fellowship of the Ring" ,
-    author: "J.R.R. Tolkein",
-        pages: 300},
-        {name: "Harry Potter",   
-    title: "The prisoner of Azcabane",
-    author: "JK Rowling",
-    pages: 200},
-    {name: "Lord of the Rings",
-    title: "The Fellowship of the Ring" ,
-    author: "J.R.R. Tolkein",
-        pages: 300}
+const myLibrary = [
+  {
+    name: "Alice",
+    title: "in wonderland",
+    author: "me!",
+    pages: 12,
+    status: "/",
+    id: "",
+  },
 ];
 
-class Books {
-    constructor(name, title, author, pages) {
-        this.name = name;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        
-    }
+class Book {
+  constructor(name, title, author, pages, status, id) {
+    this.name = name;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.id = id;
+  }
 }
 
- function addBookToLibrary() {
+openBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-    displayBooks.innerHTML = "";
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const bookName = bookNameInput.value;
+  const bookTitle = bookTitleInput.value;
+  const author = authorInput.value;
+  const pages = bookPagesInput.value;
+
+  const book = new Book(bookName, bookTitle, author, pages, "", "");
+  myLibrary.push(book);
+  displayBook();
+  console.log(book);
+  dialog.close();
+});
+
+function displayBook() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    // make sure every book get an id.
+    if (!myLibrary[i].id) {
+      myLibrary[i].id += crypto.randomUUID();
+    }
+
+    if (mySelection.value === "read") {
+      myLibrary[i].status += "READ";
+    } else if (mySelection.value === "notRead") {
+      myLibrary[i].status += "Not Read";
+    }
+
+    display.innerHTML += `<tr>
+    <td>${myLibrary[i].name}</td>
+    <td>${myLibrary[i].title}</td>
+    <td>${myLibrary[i].author}</td>
+    <td>${myLibrary[i].pages}</td>
+    <td>${myLibrary[i].status}</td>
+    <td>${myLibrary[i].id}</td>
     
-    for (let i = 0; i < myLibrary.length; i++) {
-        
-        if (!myLibrary[i].id){
-            myLibrary[i].id = crypto.randomUUID();
-        }
-
-        displayBooks.innerHTML +=
-
-           ` <tr>
-
-                <td>${myLibrary[i].name}</td>
-                <td>${myLibrary[i].title}</td>
-                <td>${myLibrary[i].author}</td>
-                <td>${myLibrary[i].pages}</td>
-                <td>${myLibrary[i].id}</td>
-
-
-            </tr>`
-        ;
-    }
-
+    </tr>`;
+  }
 }
-
-
-// const book1 = new Books("Harry Potter", "The prisoner of Azcabane", "JK Rowling", 200);
-// const book2 = new Books("Lord of the Rings", "The Fellowship of the Ring", "J.R.R. Tolkein", 300);
-
-
-addBookToLibrary();
-// book2.addBookToLibrary();
-
